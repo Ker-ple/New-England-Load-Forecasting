8b1838d9327f4f1415ee9388430be2c073ca90da
Ker-ple <cyruskirbus@gmail.com>
2023-02-14 23:15:24 -0500

    improves output date list formatting

--- Terraform/aux_functions/split_date_range/split_date_range.py
+++ Terraform/aux_functions/split_date_range/split_date_range.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
+from datetime import datetime
 
 def lambda_handler(event, context=None):
     print(event)
@@ -8,7 +9,10 @@ def lambda_handler(event, context=None):
     # a new lambda function is invoked every 30 dates in consideration. each lambda is given equal share of dates to scrape.
     num_lambdas = math.ceil(len(dates)/30)
     # returns first and last date of each sub-list is returned to save message space and because the invoked lambda functions can recreate the date range themselves.
-    return [(x.tolist()[0], x.tolist()[-1]) for x in np.array_split(dates, num_lambdas)]
+    return [{
+        'date_begin': datetime.strptime(x.tolist()[0], '%Y%m%d').strftime('%Y-%m-%d'), 
+        'date_end': datetime.strptime(x.tolist()[-1], '%Y%m%d').strftime('%Y-%m-%d')
+        } for x in np.array_split(dates, num_lambdas)]
 
 def define_yyyymmdd_date_range(start, end):
     # days in this format because the iso-ne api requires it for dates
