759150b320b69b7e076a54a1cf3c366dc0f4ccc3
Ker-ple <cyruskirbus@gmail.com>
2023-02-14 23:00:34 -0500

    adds missing weather dockerfile

--- Terraform/set_up.sh
+++
@@ -1,6 +0,0 @@
-export TF_VAR_db_username=$(pass db_username)
-export TF_VAR_db_password=$(pass db_password)
-export TF_VAR_iso_ne_auth=$(pass iso_ne_auth)
-export TF_VAR_db_name=$(pass db_name)
-export TF_VAR_weather_apis_auth=$(pass weather_apis_auth)
-terraform apply -auto-approve
