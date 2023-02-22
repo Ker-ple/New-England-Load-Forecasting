a735d751daf7aa35a178ef26e07b6bfdff7d8ac1
Ker-ple <cyruskirbus@gmail.com>
2023-02-22 11:23:45 -0500

    adds run_mode and a lambda to make new dates

--- Terraform/aux_functions/split_date_range/split_date_range.py
+++ Terraform/aux_functions/split_date_range/split_date_range.py
@@ -9,11 +9,12 @@ def lambda_handler(event, context=None):
     # a new lambda function is invoked every 30 dates in consideration. each lambda is given equal share of dates to scrape.
     num_lambdas = math.ceil(len(dates)/30)
     # returns first and last date of each sub-list is returned to save message space and because the invoked lambda functions can recreate the date range themselves.
-    return [{
-        'date_begin': datetime.strptime(x.tolist()[0], '%Y%m%d').strftime('%Y-%m-%d'), 
-        'date_end': datetime.strptime(x.tolist()[-1], '%Y%m%d').strftime('%Y-%m-%d')
-        } for x in np.array_split(dates, num_lambdas)]
+    return json.dumps([{
+        'date_begin': datetime.strptime(x.tolist()[0], '%Y%m%d').strftime('%Y%m%d'), 
+        'date_end': datetime.strptime(x.tolist()[-1], '%Y%m%d').strftime('%Y%m%d')
+        } for x in np.array_split(dates, num_lambdas)], event['run_mode'])
 
 def define_yyyymmdd_date_range(start, end):
     # days in this format because the iso-ne api requires it for dates
     return [d.strftime('%Y%m%d') for d in pd.date_range(start, end)]
+
