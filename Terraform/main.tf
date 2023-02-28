f51b3f4f1a4f37588135f69f48a0b1108c42d3dd
Ker-ple <cyruskirbus@gmail.com>
2023-02-27 21:11:06 -0500

    fixes incorrect arn references

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,12 @@
 locals {
-    config_iterate_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.config_iterate_lambda.id}"
-    config_uscrn_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.config_uscrn_lambda.id}"
-    config_forecasts_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.config_forecasts_lambda.id}"
-    config_iso_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.config_iso_lambda.id}"
-    iso_load_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.iso_load_lambda.id}"
-    iso_forecast_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.iso_forecast_lambda.id}"
-    uscrn_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.uscrn_lambda.id}"
-    pirate_weather_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:${random_pet.weather_forecast_lambda.id}"
+    config_iterate_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.config_iterate_lambda.id}"
+    config_uscrn_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.config_uscrn_lambda.id}"
+    config_forecasts_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.config_forecasts_lambda.id}"
+    config_iso_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.config_iso_lambda.id}"
+    iso_load_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.iso_load_lambda.id}"
+    iso_forecast_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.iso_forecast_lambda.id}"
+    uscrn_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.uscrn_lambda.id}"
+    pirate_weather_lambda_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.this.account_id}:function:${random_pet.weather_forecast_lambda.id}"
 }
 
 module "iso_step_function" {
@@ -30,7 +30,7 @@ module "iso_step_function" {
             effect = "Allow",
             actions = ["lambda:InvokeFunction"],
             resources = [
-                "${local.config_iso_lambda}:*",
+                "${local.config_iso_lambda_arn}:*",
                 "${local.iso_forecast_lambda_arn}:*",
                 "${local.iso_load_lambda_arn}:*",
                 "${local.config_iterate_lambda_arn}:*"
@@ -50,7 +50,7 @@ module "uscrn_step_function" {
         "${path.root}/state_machines/USCRN.asl.yaml.tftpl", {
         "config_uscrn_lambda_arn" = local.config_uscrn_lambda_arn
         "uscrn_lambda_arn" = local.uscrn_lambda_arn
-        "iterate_lambda_arn" = local.config_iterate_lambda_arn
+        "config_iterate_lambda_arn" = local.config_iterate_lambda_arn
     })))
 
     attach_policy_statements = true
@@ -78,7 +78,7 @@ module "pirate_step_function" {
         "${path.root}/state_machines/PIRATE.asl.yaml.tftpl", {
         "config_forecasts_lambda_arn" = local.config_forecasts_lambda_arn
         "pirate_weather_lambda_arn" = local.pirate_weather_lambda_arn
-        "iterate_lambda_arn" = local.config_iterate_lambda_arn
+        "config_iterate_lambda_arn" = local.config_iterate_lambda_arn
     })))
 
     attach_policy_statements = true
